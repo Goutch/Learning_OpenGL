b0751d5573ba127a7ab77c5ed0d23d148de3721f
gabriel bouchard <gaboutch@outlook.com>
2020-02-01 17:54:12 -0500

    Update LAB2.h

--- src/engine/Test/LAB2.h
+++ src/engine/Test/LAB2.h
@@ -14,7 +14,7 @@ class LAB2 : public Scene{
     AtlasTexture font = AtlasTexture("../res/font.bmp", 64, 64, false);
     Material material = Material(shader, font);
     Material defaultMat = Material(shader);
-
+    unsigned int counter=0;
     Mesh vao;
 public:
     void init(Viewport &viewport, Renderer &renderer, Window &window) override;
