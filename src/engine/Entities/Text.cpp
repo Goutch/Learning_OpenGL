7869a242cfb7fb227d21d89d63de407ee4c47e64
gabriel bouchard <gaboutch@outlook.com>
2020-02-04 16:05:01 -0500

    merge

--- src/engine/Entities/Text.cpp
+++ src/engine/Entities/Text.cpp
@@ -6,10 +6,11 @@
 #include "Core/Scene.h"
 
 
-Text::Text(std::string text, const vec3 &position, FontMaterial &font) : Entity(position),font(font) {
+Text::Text(std::string text, const vec3 &position, FontMaterial &font) : Entity(position), font(font) {
     Geometry::make_text(mesh, text, font);
 }
+
 void Text::draw(const Scene &scene) const {
     Entity::draw(scene);
-    scene.getRenderer().draw(mesh, font, transform);
+    scene.getRenderer().drawUI(mesh, transform, font);
 }
--- src/engine/Test/TextTest.cpp
+++ src/engine/Test/TextTest.cpp
@@ -13,18 +13,9 @@
 
 void TextTest::init(Viewport &viewport, Renderer &renderer, Window &window) {
     Scene::init(viewport, renderer, window);
-    camera->setProjectionMode(Camera::ProjectionMode::ORTHOGRAPHIC_UNITS);
     //Geometry::make_text(vao, "test", font);
     addEntity(new Text("test", vec3(0),  font));
 }
 void TextTest::render() const {
-    renderer->drawUI(vao, transform, font);
     Scene::render();
-    //renderer->draw(vao, font, transform);
-}
-
-void TextTest::onKeyPress(char key) {
-    float uvs[8];
-    font.getCoordinates(uvs,key-32);
-    vao.uvs(uvs, 8);
 }
