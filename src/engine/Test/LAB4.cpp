54d3ff91f9a96bd9469ac89df0be69366b8cdd6c
Simferoce <le6monstre@hotmail.com>
2020-02-10 16:33:11 -0500

    LAB4

--- src/engine/Test/LAB4.cpp
+++ src/engine/Test/LAB4.cpp
@@ -7,27 +7,28 @@
 #include <Core/Canvas.h>
 #include "Entities/Canvas/Line.h"
 
-int maxGen = 1;
+int maxGen = 8;
 void LAB4::init(const Canvas &canvas, Renderer &renderer, Window &window) {
     Scene::init(canvas, renderer, window);
     float w = canvas.getPixelWidth();
     float h = canvas.getPixelHeight();
-    addEntity(new Line(vec2(w/2, h/2) , vec2(w/2, 50), 4, material));
+    fractale(w/2,0,1);
 }
 
 void LAB4::draw() const {
     Scene::draw();
-    float w = canvas->getPixelWidth();
-    float h = canvas->getPixelHeight();
-    renderer->drawLine(w/2, h/2, w/2, 50, 4, Color::RED);
 }
 
 void LAB4::fractale(float lastX, float lastY, int gen) {
     if(gen == maxGen + 1)
         return;
 
-    float newX = lastX;
-    float newY = lastY + 10;
-    addEntity(new Line(vec2(lastX, lastY) , vec2(newX, newY), 4/(gen/4 + 1), material));
+    float newX = lastX + 200*(1.0f/gen);
+    float newY = lastY + 200*(1.0f/gen);
+    addEntity(new Line(vec2(lastX, lastY) , vec2(newX, newY), ((float)maxGen + 1 - gen)/maxGen*3 + 1, material));
     fractale(newX, newY, gen + 1);
+    float newX2 = lastX - 200*(1.0f/gen);
+    float newY2 = lastY + 200*(1.0f/gen);
+    addEntity(new Line(vec2(lastX, lastY) , vec2(newX2, newY2), ((float)maxGen + 1 - gen)/maxGen*3 + 1, material));
+    fractale(newX2, newY2, gen + 1);
 }
