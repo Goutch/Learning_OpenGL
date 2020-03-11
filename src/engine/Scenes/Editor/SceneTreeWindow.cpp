c8844322e70ae4b36f17481d5db66ccfb1bd49d7
gabriel bouchard <gaboutch@outlook.com>
2020-03-11 15:37:30 -0400

    update tree

--- src/engine/Scenes/Editor/SceneTreeWindow.cpp
+++ src/engine/Scenes/Editor/SceneTreeWindow.cpp
@@ -32,55 +32,54 @@ void SceneTreeWindow::createEntityTree(Input& input,Entity *entity,std::set<Enti
         ImGui::Bullet();
         ImGui::SameLine();
         ImGui::Selectable(entity->getName().c_str(), is_selected);
-        if (ImGui::IsItemClicked()) {
-            //if multiselect
-            if (input.isKeyDown(GLFW_KEY_LEFT_CONTROL)) {
-                if(is_selected)
-                {
-                    selected_entities.erase(entity);
-                }
-                if(!is_selected)
-                {
-                    selected_entities.insert(entity);
-                }
-            }
-            //no multiselect
-            else
-            {
-                selected_entities.clear();
-                selected_entities.insert(entity);
-            }
-
-
-        }
-
+        handleClick(is_selected,input,entity,selected_entities);
     } else {
         ImVec2 pos = ImGui::GetCursorScreenPos();
         ImU32 col = ImColor(ImGui::GetStyle().Colors[ImGuiCol_TextSelectedBg]);
         //if tree node is open
+
         if (ImGui::TreeNode(entity->getName().c_str())) {
+            handleClick(is_selected,input,entity,selected_entities);
             for (auto child:entity->getChildren()) {
                 createEntityTree(input,child,selected_entities);
             }
             ImGui::TreePop();
         }
-
-        if (!is_selected && ImGui::IsItemClicked()) {
-            if (!input.isKeyDown(GLFW_KEY_LEFT_CONTROL)) {
-                selected_entities.clear();
-            }
-            selected_entities.insert(entity);
-        }
         //create rect to indicate selection
         if (is_selected)
+        {
             ImGui::GetWindowDrawList()->AddRectFilled(pos, ImVec2(pos.x + ImGui::GetContentRegionMax().x,
                                                                   pos.y + ImGui::GetTextLineHeight()), col, false);
+        }
+
     }
 }
-
+void SceneTreeWindow::handleClick(bool is_selected,Input &input, Entity *entity, std::set<Entity *> &selected_entities) const{
+    if (ImGui::IsItemClicked()&&(ImGui::GetMousePos().x - ImGui::GetItemRectMin().x) > ImGui::GetTreeNodeToLabelSpacing()) {
+        //if multiselect
+        if (input.isKeyDown(GLFW_KEY_LEFT_CONTROL)) {
+            if(is_selected)
+            {
+                selected_entities.erase(entity);
+            }
+            if(!is_selected)
+            {
+                selected_entities.insert(entity);
+            }
+        }
+            //no multiselect
+        else
+        {
+            selected_entities.clear();
+            selected_entities.insert(entity);
+        }
+    }
+}
 void SceneTreeWindow::update(Input& input,ImGuiID dockspace_id,const std::vector<Entity*>& entities,std::set<Entity *>& selected_entities) {
     ImGui::SetNextWindowDockID(dockspace_id, ImGuiCond_FirstUseEver);
     ImGui::Begin("Scene");
     createHierachy(input,entities,selected_entities);
     ImGui::End();
 }
+
+
--- src/engine/Scenes/Editor/SceneTreeWindow.h
+++ src/engine/Scenes/Editor/SceneTreeWindow.h
@@ -8,7 +8,7 @@
 class SceneTreeWindow {
     void createHierachy(Input& input,const std::vector<Entity*>& entities,std::set<Entity*>& selected_entities) const;
     void createEntityTree(Input& input,Entity *entity,std::set<Entity*>& selected_entities) const;
-
+    void handleClick(bool is_selected,Input& input,Entity *entity,std::set<Entity*>& selected_entities) const;
 public:
     void update(Input& input,ImGuiID dockspace_id,const std::vector<Entity*>& entities,std::set<Entity*>& selected_entities);
 };
