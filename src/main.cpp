ddecdbc76297d2d461bd0e665e182576b487e87c
Gabriel Bouchard <gbouchard@ova.ai>
2020-06-11 09:48:29 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "Core/Engine.h"
 #include "Scenes/Test/Test.h"
 int main() {
-    Engine engine=Engine();
-    engine.run<Test>();
-    engine.start();
+  Engine engine=Engine();
+  engine.run<Test>();
+  engine.start();
     return 0;
 }
